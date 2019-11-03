407ab40a04e87a98d4bc7e60aa3f98160418071f
Greg Ward <greg@gerg.ca>
2019-11-03 14:50:11 -0500

    Add multithreaded test for Python version
    Loggers are technically not threadsafe, because they have mutable
    state and no locks. But they appear to behave OK right now, because
    the current code doesn't really care how multiple threads interleave
    in _log(). That's just an accident of implementation, though!

--- python/lolog.py
+++ python/lolog.py
@@ -133,14 +133,20 @@ def get_logger(name):
 
 
 def main():
+    config = get_config()
+    config.set_outfile(sys.stdout)
+    config.add_context("time", isotime)
+    config.set_logger_level("lib.guts", Level.INFO)
+
+    simple_test()
+    mt_test()
+
+
+def simple_test():
+    print("** START simple_test() **")
     applog = get_logger("myapp")
     liblog = get_logger("lib.guts")
     print(f"applog = {applog!r}, liblog = {liblog!r}")
-
-    config = get_config()
-    config.add_context("time", isotime)
-
-    liblog.level = Level.INFO
 
     liblog.debug("this is a really chatty library", arg1="bla", arg2="hi")
     applog.debug("hello from the app")
@@ -151,6 +157,38 @@ def main():
     applog.add_context("request_id", "244a")
     applog.info("useful info from the app")
 
+    assert get_logger("myapp") is applog
+
+    subliblog = get_logger("lib.guts.deep")
+    subliblog.debug("this sublib is also noisy", arg="!!!")
+    print("** END simple_test() **")
+
+
+def mt_test():
+    print("** START mt_test() **")
+
+    num_threads = 10
+    log = get_logger("myapp.mt")
+    log.add_context("threads", str(num_threads))
+
+    def worker(name):
+        tid = threading.current_thread().ident
+        count = random.randint(10, 20)
+        for idx in range(1, count + 1):
+            message = f"message {idx}/{count} from {name} ({tid})"
+            log.info(message, idx=idx, count=count)
+
+    threads = []
+    for idx in range(num_threads):
+        name = f"thread {idx}"
+        thread = threading.Thread(target=worker, name=name, args=(name,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    print("** END mt_test() **")
 
 
 if __name__ == "__main__":
