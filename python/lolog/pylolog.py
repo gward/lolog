gward/lolog

commit 22af538a7b9746a439a83011afe76343e26dbaa1

First crack at intercepting calls to standard logging module
This is very rough and preliminary and doesn't come close to
handling everything.

python/lolog/pylolog.py
@@ -7,7 +7,7 @@ import enum
 import sys
 import threading
 import time
-from typing import ClassVar, Optional, Callable, Dict, List, Tuple, TextIO
+from typing import ClassVar, Optional, Any, Callable, Dict, List, Tuple, TextIO
 
 
 class Level(enum.IntEnum):
@@ -118,20 +118,20 @@ class Logger:
     def add_context(self, key, value):
         self.context.append((key, value))
 
-    def debug(self, message: str, **items):
-        self._log(Level.DEBUG, message, **items)
+    def debug(self, message: str, **kwargs):
+        self._log(Level.DEBUG, message, **kwargs)
 
-    def info(self, message: str, **items):
-        self._log(Level.INFO, message, **items)
+    def info(self, message: str, **kwargs):
+        self._log(Level.INFO, message, **kwargs)
 
-    def _log(self, level: Level, message: str, **items):
+    def _log(self, level: Level, message: str, items: List[Tuple[str, Any]]):
         config = self.config
 
         context = [
             *config.get_context(),
             *config.get_local_context(),
             *self.context,
-            *items.items(),
+            *items,
         ]
         record = Record(
             time=config.time(),

python/intercept-test.py
@@ -1,0 +1,23 @@
+"""informal manual test of interception"""
+
+import logging
+import lolog
+import lolog.iclogging
+
+#log = logging.getLogger()
+
+
+def main():
+    cfg = lolog.init()
+    lolog.iclogging.intercept(cfg)
+
+    logging.getLogger().info("info message to root logger")
+
+    logging.getLogger("foo").debug("debug message to foo")
+    logging.getLogger("foo.bar").info("info message with arg %s", "meep")
+    logging.getLogger("foo.baz").debug("debug with arg %s and kwargs %(bar)s",
+                                       "blip", bar=42)
+
+
+main()
+

python/lolog/iclogging.py
@@ -1,0 +1,75 @@
+"""intercept calls to the standard logging module"""
+
+import threading
+
+from .pylolog import Config, Level
+
+
+class InterceptManager:
+    """replacement for logging.Manager class"""
+
+    def __init__(self, cfg):
+        self.mutex = threading.Lock()
+        self.cfg = cfg
+        self.logger = {}
+
+    def getLogger(self, name):
+        with self.mutex:
+            if name not in self.logger:
+                self.logger[name] = InterceptLogger(self.cfg, name)
+            return self.logger[name]
+
+
+class InterceptLogger:
+    """replacement for logging.Logger class"""
+    def __init__(self, cfg, name):
+        self.logger = cfg.get_logger(name)
+
+    def debug(self, msg, *args, **kwargs):
+        self._log(Level.DEBUG, msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        self._log(Level.INFO, msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        self._log(Level.WARNING, msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        self._log(Level.ERROR, msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        self._log(Level.CRITICAL, msg, *args, **kwargs)
+
+    def exception(self, msg, *args, exc_info=True, **kwargs):
+        pass                    # ummm...
+
+    def _log(self, level, msg, *args, **kwargs):
+        items = [("arg%d" % (i + 1), arg)
+                 for (i, arg) in enumerate(args)]
+        items += kwargs.items()
+        self.logger._log(level, msg, items)
+
+
+_save_manager = None
+_save_root = None
+
+
+def intercept(cfg: Config):
+    global _save_manager, _save_root
+    import logging
+
+    _save_manager = logging.Logger.manager
+    _save_root = logging.Logger.root
+
+    manager = InterceptManager(cfg)
+    logging.Logger.manager = manager
+    logging.Logger.root = manager.getLogger("")
+
+
+def undo_intercept():
+    global _save_manager, _save_root
+    import logging
+
+    logging.Logger.manager = _save_manager
+    logging.Logger.root = _save_root
+    _save_manager = _save_root = None

commit ffbfa14d31db352276e364b426c6f2447f7d9313

Fix so basic logging actually works again
Regression introduced in 22af538a7b9746a439a83011afe76343e26dbaa1.

python/lolog/pylolog.py
@@ -119,10 +119,10 @@ class Logger:
         self.context.append((key, value))
 
     def debug(self, message: str, **kwargs):
-        self._log(Level.DEBUG, message, **kwargs)
+        self._log(Level.DEBUG, message, kwargs.items())
 
     def info(self, message: str, **kwargs):
-        self._log(Level.INFO, message, **kwargs)
+        self._log(Level.INFO, message, kwargs.items())
 
     def _log(self, level: Level, message: str, items: List[Tuple[str, Any]]):
         config = self.config